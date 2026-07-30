5dea49b1c43e875a387d21a42bfb3141920da1ab
BeigeCocer <1700580494@qq.com>
2026-07-30 14:37:34 +0800

    更新站点配置和添加 wrangler 配置

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,27 +6,27 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "箱庭世界·尤雷忒",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "欢迎回来 绘川小姐",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://394000.xyz",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"这是其名为可能性的世界，跨越三个纪元的伟业与篇章，自无垠的寰宇里所显现，为其带来无限的可能性……",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
-		"Fuwari",
-		"Astro",
-		"ACGN",
-		"博客",
-		"技术博客",
-		"静态博客",
+		"世界伊始？",
+		"旧世界",
+		"术语设定",
+		"平凡又传奇的生灵们",
+		"境界魔法树",
+		"其他",
+		"相关留言",
 	],
 
 	// 主题色
@@ -77,7 +77,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "箱庭世界",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
